c2bbe7c20f54e05127918dfcd8b0c6fc43a5f82e
Lukas Kalensky <lukas.kalensky@unob.cz>
2025-12-14 15:01:17 +0000

    zmena

--- app_moje/modely.py
+++ app_moje/modely.py
@@ -12,12 +12,6 @@ class User(db.Model):
 
     password_hash = db.Column(db.String(128), nullable=False)
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
     @property
     def password(self):
         raise AttributeError("Password is write-only")  # getter jen na ochranu
